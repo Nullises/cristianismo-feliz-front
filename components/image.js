847acaf67c719f6dc1499556fbfc2a60a3a9f71d
Ulises Vargas <ext_uvargasds@falabella.cl>
2023-09-01 20:49:24 -0400

    fix/rollback

--- components/image.js
+++ components/image.js
@@ -5,17 +5,14 @@ const Image = ({ image }) => {
   const { alternativeText, width, height, url } = image.data.attributes;
 
   return (
-    <NextImage
-      layout="responsive"
+    <img
       src={
         url.startsWith("http")
           ? url
           : `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${url}`
       }
-      objectFit="contain"
-      alt={alternativeText || ""}
-      width={width ? width : 600}
-      height={height ? height : 400}
+      width={width}
+      height={height}
     />
   );
 };
