bbb29a9786808ab0df9025f2ea90032a6a3b58f9
Ulises Vargas <ext_uvargasds@falabella.cl>
2023-09-04 10:03:00 -0400

    fix/img-next-pattern

--- components/image.js
+++ components/image.js
@@ -2,20 +2,16 @@ import { getStrapiMedia } from "../lib/media";
 import NextImage from "next/image";
 
 const Image = ({ image }) => {
-  const { alternativeText, width, height, url } = image.data.attributes;
+  const { alternativeText, width, height } = image.data.attributes;
 
   return (
     <NextImage
       layout="responsive"
-      src={
-        url.startsWith("http")
-          ? url
-          : `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${url}`
-      }
+      width={width}
+      height={height}
       objectFit="contain"
+      src={getStrapiMedia(image)}
       alt={alternativeText || ""}
-      width={width ? width : 600}
-      height={height ? height : 400}
     />
   );
 };
