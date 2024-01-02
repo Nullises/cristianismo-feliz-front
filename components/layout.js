5266ab97af3518aaec0ea476df2b11b376dbe36f
Ulises Vargas <ext_uvargasds@falabella.cl>
2024-01-02 15:27:05 -0400

    feature/speed-insights

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Nav from "./nav";
 import Footer from "./footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const Layout = ({ children, categories, seo }) => (
   <>
     <Nav categories={categories} />
     {children}
+    <SpeedInsights />
     <Footer></Footer>
   </>
 );
